7bfd8a7144ab1659dd6bfdf22579c1dc51474d6a
Marco <802019+MMarco94@users.noreply.github.com>
2026-04-06 10:36:45 +0100

    Optimized shader

--- src/main/kotlin/io/github/mmarco94/tambourine/ui/Shaders.kt
+++ src/main/kotlin/io/github/mmarco94/tambourine/ui/Shaders.kt
@@ -1,7 +1,6 @@
 package io.github.mmarco94.tambourine.ui
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.graphics.ShaderBrush
@@ -12,24 +11,22 @@ import org.jetbrains.skia.RuntimeShaderBuilder
 
 @Language("AGSL")
 private const val paperLikeShader = """
-uniform float2 resolution;
-half4 main(float2 fragCoord) {
-    float2 uv = fragCoord / resolution;
-    float noise1 = fract(sin(dot(uv, float2(12.987, 56.123))) * 12345.6789);
-    float noise2 = fract(sin(dot(uv, float2(23.654, 67.456))) * 23456.7891);
-    float noise3 = fract(sin(dot(uv, float2(34.321, 78.789))) * 34567.8912);
+half4 main(float2 uv) {
+    float noise1 = fract(sin(dot(uv, float2(0.012987, 0.056123))) * 12345.6789);
+    float noise2 = fract(sin(dot(uv, float2(0.023654, 0.067456))) * 23456.7891);
+    float noise3 = fract(sin(dot(uv, float2(0.034321, 0.078789))) * 34567.8912);
     return 0.05 * half4(noise1, noise2, noise3, 0.0);
 }"""
 
+private val paperLikeShaderBrush = ShaderBrush(
+    RuntimeShaderBuilder(RuntimeEffect.makeForShader(paperLikeShader))
+        .makeShader()
+)
+
 @Composable
 fun Modifier.paperNoise(): Modifier {
-    val shaderBuilder = remember(paperLikeShader) {
-        RuntimeShaderBuilder(RuntimeEffect.makeForShader(paperLikeShader))
-    }
     return drawWithContent {
         drawContent()
-        shaderBuilder.uniform("resolution", this.size.width, this.size.height)
-        val brush = ShaderBrush(shaderBuilder.makeShader())
-        drawRect(brush)
+        drawRect(paperLikeShaderBrush)
     }
 }
