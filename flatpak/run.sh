3805fe0b68d0578934e8cfa2eb368a876ba16c5b
Marco <802019+MMarco94@users.noreply.github.com>
2026-05-16 12:56:59 +0100

    Set MALLOC_TRIM_THRESHOLD_ on test flatpak runner

--- flatpak/run.sh
+++ flatpak/run.sh
@@ -2,14 +2,16 @@
 
 set -ex
 
+ # Printing the local time, as a reference to measure startup performances
+date +"%T.%N"
+
 # Setting up tmp dir
 rm -f "$XDG_CACHE_HOME"/tmp/*.png
 mkdir -p "$XDG_CACHE_HOME"/tmp
 
 export JAVA_TOOL_OPTIONS="-Djava.io.tmpdir=$XDG_CACHE_HOME/tmp -Djava.util.prefs.userRoot=$XDG_CONFIG_HOME"
-
- # Printing the local time, as a reference to measure startup performances
-date +"%T.%N"
+#export MALLOC_ARENA_MAX=8
+export MALLOC_TRIM_THRESHOLD_=131072 # 128k
 # Printing the JAR's stat for debugging
 stat /app/tambourine/lib/app/tambourine-*.jar
 
